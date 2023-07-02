442a7197b81c10940a66e02dc45202cdbec4e1cb
123wowow123 <tech@chronopin.com>
2023-07-02 09:52:36 -0700

    .

--- client/app/search/search.js
+++ client/app/search/search.js
@@ -4,6 +4,16 @@ angular.module('chronopinNodeApp')
   .config(function($stateProvider) {
     $stateProvider.state('search', {
       url: '/search?q&f',
-      template: '<search></search>'
+      template: '<search></search>',
+      
+      onEnter: () => {
+        // scroll to top
+        setTimeout(() => {
+          document.documentElement.scrollTo({
+            top: 0
+          });
+        }, 0);
+      }
+
     });
   });
