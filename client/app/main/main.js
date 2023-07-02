020ad27fdda7fbc78ad4aad80c6b448fc29aac0b
123wowow123 <tech@chronopin.com>
2023-07-02 11:36:55 -0700

    .

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -1,9 +1,19 @@
 'use strict';
 
 angular.module('chronopinNodeApp')
-  .config(function($stateProvider) {
+  .config(function ($stateProvider) {
     $stateProvider.state('main', {
       url: '/',
-      template: '<main></main>'
+      template: '<main></main>',
+
+      onEnter: () => {
+        // https://stackoverflow.com/questions/7131909/facebook-callback-appends-to-return-url/18305085#18305085
+        if (window.location.hash === "#_=_") {
+          history.replaceState
+            ? history.replaceState(null, null, window.location.href.split("#")[0])
+            : window.location.hash = "";
+        }
+      }
+
     });
   });
